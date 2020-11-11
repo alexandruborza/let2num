cab4d882f89c7e9735f56a47b0dcc6ea9e1428fb
Alexandru Borza <3216845+alexandruborza@users.noreply.github.com>
2020-11-11 13:20:05 +0100

    Update lettwonum.php

--- lettwonum.php
+++ lettwonum.php
@@ -1,4 +1,4 @@
-function($let) {
+function let2num($let) {
 	$search = ['A' ,'B' ,'E' ,'G' ,'I' ,'O' ,'P' ,'S' ,'T' ,'Z'];
 	$replace = ['4' ,'8' ,'3' ,'6' ,'1' ,'0' ,'9' ,'5' ,'7' ,'2'];
 	$subject = strtoupper($let);
